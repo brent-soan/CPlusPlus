d85a62433cc37699dc20178f13ef0462dc44e98f
brent-soan <147591053+brent-soan@users.noreply.github.com>
2025-06-11 21:53:50 +0800

    Added fmt to Libraries

---
+++ Libraries/fmt/fmt.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <fmt/base.h>
+#include <fmt/format.h>
+
+int main() {
+	std::string s;
+
+	fmt::print("Hello, world!\n");
+	s = fmt::format("The answer is {}.", 42);
+	fmt::print("{}\n", s);
+}		
