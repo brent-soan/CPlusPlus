08ed97d088a52a4fac0c946283f3026cb7b5269c
brent-soan <147591053+brent-soan@users.noreply.github.com>
2025-06-17 14:44:15 +0800

    Updated Window.cpp

--- Libraries/SFML/Window/Window/Window.cpp
+++ Libraries/SFML/Window/Window/Window.cpp
@@ -4,8 +4,9 @@ int main()
 {
 	//sf::Window window(sf::VideoMode({ 800, 600 }), "My window");
 	sf::Window window;
-	window.create(sf::VideoMode({ 800, 600 }), "My window");
-
+	window.create(sf::VideoMode({ 800, 600 }), "Sample window title");
+	window.setSize({ 1920, 1080 });
+	window.setTitle("Sample window title 2");
 	while (window.isOpen())
 	{
 		while (const std::optional event = window.pollEvent())
