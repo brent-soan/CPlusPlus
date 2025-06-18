e67f414248a6148605eb7024397f605402a088af
brent-soan <147591053+brent-soan@users.noreply.github.com>
2025-06-18 18:52:14 +0800

    Updated Events.cpp

--- Libraries/SFML/Events/Events/Events.cpp
+++ Libraries/SFML/Events/Events/Events.cpp
@@ -14,20 +14,18 @@ int main()
 				window.close();
 			}
 
-			if (event->is<sf::Event::Resized>()) {
+			// - Declares resized variable and initialize with value of event->getIf<sf::Event::Resized>()
+			// - Code inside if block will only execute if value is of type sf::Event::Resized and not nullptr
+			// - resized variable is only valid inside the if block
+			if (const auto* resized = event->getIf<sf::Event::Resized>()) {
 				std::cout << "Window resized\n";
-
-				if (const auto* resized = event->getIf<sf::Event::Resized>()) {
-					std::cout << "New height: " << resized->size.x << "\n";
-					std::cout << "New width: " << resized->size.y << "\n";
-				}
+				std::cout << "New height: " << resized->size.x << "\n";
+				std::cout << "New width: " << resized->size.y << "\n";
 			}
 
-			if (event->is<sf::Event::TextEntered>()) {
-				if (const auto* textEntered = event->getIf<sf::Event::TextEntered>()) {
-					if (textEntered->unicode < 128) {
-						std::cout << static_cast<char>(textEntered->unicode) << "\n";
-					}
+			if (const auto* textEntered = event->getIf<sf::Event::TextEntered>()) {
+				if (textEntered->unicode < 128) {
+					std::cout << static_cast<char>(textEntered->unicode) << "\n";
 				}
 			}
 		}
