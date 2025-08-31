5251b87f1bc0c7c2deb556f6725cf85a05e046be
brent-soan <147591053+brent-soan@users.noreply.github.com>
2025-08-31 17:23:38 +0800

    Update dotenv-cpp files

--- Libraries/dotenv-cpp/main.cpp
+++ Libraries/dotenv-cpp/main.cpp
@@ -4,9 +4,21 @@
 #include <string>  
 #include <dotenv.h>
 
-int main()  
-{  
+int main() {  
     dotenv::init(".env");  
-    std::cout << std::getenv("KEY") << std::endl; // Unsafe in Visual Studio
-    return 0;  
+	const char* key = std::getenv("KEY"); // getenv() returns a pointer to the string or nullptr if not found
+
+	try {
+		if(key) {
+			std::cout << "KEY = " << key << std::endl;
+		}
+		else {
+			throw std::runtime_error("KEY was not found");
+		}
+	}
+	catch(const std::runtime_error& e) {
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+
+	return 0;
 }
